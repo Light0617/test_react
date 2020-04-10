f0e86eeb039b3fa63aa695cf37fe7012fff588a0
light <khl085@ucsd.edu>
2020-04-10 00:42:34 -0700

    Updates

---
+++ precache-manifest.0eaa7e230390e3f6414e3b9ee799cf5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99902f4016581e2fd5833230edabf010",
+    "url": "/test_react/index.html"
+  },
+  {
+    "revision": "f4501055ad35a3e5f59f",
+    "url": "/test_react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ea99420d74ee7acd1249",
+    "url": "/test_react/static/css/main.d05eb81f.chunk.css"
+  },
+  {
+    "revision": "f4501055ad35a3e5f59f",
+    "url": "/test_react/static/js/2.00b6e822.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/test_react/static/js/2.00b6e822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea99420d74ee7acd1249",
+    "url": "/test_react/static/js/main.2511bbf1.chunk.js"
+  },
+  {
+    "revision": "2e554163c089e4a891bf",
+    "url": "/test_react/static/js/runtime-main.1dce6781.js"
+  },
+  {
+    "revision": "b5724770d724d9b8dd10077b881a35db",
+    "url": "/test_react/static/media/101.b5724770.png"
+  }
+]);
